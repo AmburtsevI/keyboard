f4b0d87895acd890aec252e629baaf0d700650ee
Igor Amburtsev <iamburtsev@dpg.email>
2024-05-21 18:38:43 +0500

    Init svelte and create a layout

---
+++ src/lib/VirtualKeyboard.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    export let layout:string[][] = [];
+</script>
+
+<div class=keyboardWrapper>
+    <div class="keyboard">
+        {#each layout as row}
+            <div class="row">
+                {#each row as key}
+                    <button>{key}</button>
+                {/each}
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style lang="scss">
+
+    // .keyboardWrapper {
+        // width: 360px;
+        // display: flex;
+    // }
+    .keyboard {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+    }
+
+    .row {
+        display: flex;
+        gap: 6px;
+        // row-gap: 2px;
+    }
+
+    button {
+        width: 78px;
+        height: 63px;
+        box-sizing: border-box;
+        padding: 0px 5px 5px 5px;
+        // border: none;
+        font-size: 30px;
+        line-height: 30px;
+        border-radius: 0px;
+    }
+</style>
+  
---
+++ src/lib/VirtualKeyboardWrapper.ts
@@ -0,0 +1,11 @@
+import VirtualKeyboard from './VirtualKeyboard.svelte';
+
+export default class VirtualKeyboardWrapper {
+    layout!: [[string]];
+
+    constructor() {}
+
+    handleKeyPress(event) {
+        // Handle key press event
+    }
+}
